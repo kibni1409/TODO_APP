64e3971abea3af343d4aa52d06f504bdc1aa536e
kibni <kibni1409@gmail.com>
2023-02-06 13:50:40 +0300

    Func Data

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,4 +1,5 @@
 import React from "react";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import "./Task.css"
 function Task ( props ) {
     return (
@@ -7,12 +8,14 @@ function Task ( props ) {
                 <input className="toggle" type="checkbox"></input>
                     <label>
                         <span className="description">{ props.description }</span>
-                        <span className="created">created 17 seconds ago</span>
+                        <span className="created">
+                            { formatDistanceToNow(new Date(2023, 1, 6, 13, 49, 15)) }
+                        </span>
                     </label>
                     <button className="icon icon-edit"></button>
                     <button className="icon icon-destroy"></button>
             </div>
-            { props.type === "editing" ? <input type="text" onChange={ () => {} } className="edit" value={ props.description }></input> : null }
+            { props.type === "editing" ? <input type="text" className="edit" value={ props.description }></input> : null }
         </li>
     )
 }
